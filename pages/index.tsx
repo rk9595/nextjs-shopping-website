aa8fc1dd6af0705da5f765338c5cc03d3b3358cc
rk9595 <83279947+rk9595@users.noreply.github.com>
2023-02-05 15:06:52 +0530

    feat: headerdone

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Apple Redesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
     </div>
   );
 };
